fd94b15d677de9909181157fc5d0b2965164e7f1
andrej <andrejsaule8@gmail.com>
2020-09-24 15:53:52 +1000

    somewhat working search-bar dropdown

--- src/components/MainSearch.js
+++ src/components/MainSearch.js
@@ -4,10 +4,22 @@ import './MainSearch.scss';
 
 export default class MainSearch extends Component {
 
-    handleChange = (e) => {
-
+    state = {
+        mainSearchDropdown: false
     }
-    
+
+
+    handleChange = (e) => {
+        this.setState({
+            mainSearchDropdown: true
+        })
+    }
+    handleFocusOut = (e) => {
+        this.setState({
+            mainSearchDropdown: false
+        })
+    }
+
     render() {
         const { mainSearchDropdown } = this.state;
 
@@ -29,12 +41,19 @@ export default class MainSearch extends Component {
                         <img className="poodle-hero-img" src="https://fontmeme.com/permalink/200922/16bf0f375ad5e81bd6128af3a69a0b59.png" />
                     </div>
                     <div className="search-container">
-                        <input onChange={this.handleChange} type="text" className="search-bar" />
-                        <div className="lucky-buttons">
-                            {/* these will need to be sumbit for the API  */}
-                            <input type="button" value="Poodle Search"/>
-                            <input type="button" value="I'm Feeling Lucky"/>
+                        <div className={mainSearchDropdown ? "search-bar-dropdown" : "search-bar-wrapper"} >
+                            <i className="search-icon"></i>
+                            <input
+                                onClick={this.handleChange}
+                                onBlur={this.handleFocusOut}
+                                type="text"
+                                className="search-bar" />
                         </div>
+                            <div className="lucky-buttons">
+                                {/* these will need to be sumbit for the API  */}
+                                <input type="button" value="Poodle Search"/>
+                                <input type="button" value="I'm Feeling Lucky"/>
+                            </div>
                     </div>
                     <div className="footer">
                         <span>Australia</span>
